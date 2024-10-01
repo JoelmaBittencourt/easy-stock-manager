65d61bc2a160c3c724a736eedffc4fb1ea018575
joelma <devjoelma@gmail.com>
2024-10-01 18:00:23 -0300

    feat: configuração do projeto com Spring Boot e dependências
    - Configurada a estrutura básica do projeto usando Gradle.
    - Adicionadas dependências para Spring Web, Spring Data JPA, Spring Security, Lombok e validação.
    - Adicionado README.md com descrição do projeto

---
+++ src/main/java/com/easystockmanager/EasyStockManagerApplication.java
@@ -0,0 +1,13 @@
+package com.easystockmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyStockManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyStockManagerApplication.class, args);
+	}
+
+}
